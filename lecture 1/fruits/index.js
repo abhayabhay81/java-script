30757a520f2a59c989cbab02cb08140a6af59743
abhaykumar <abhayk80120@gmail.com>
2025-02-20 15:40:49 +0530

    lecture 1

---
+++ lecture 1/fruits/index.js
@@ -0,0 +1,7 @@
+const apple = require("./apple")
+const banana = require("./banana")
+const papaya = require("./papaya")
+
+let info = [apple,banana,papaya]
+
+module.exports = info
---
+++ lecture 1/fruits/papaya.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name : "papaya",
+    color : "yellow"
+}
