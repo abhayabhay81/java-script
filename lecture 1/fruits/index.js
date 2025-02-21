6ad0f26c15302b339c814fb00871b950e19db7f0
abhayabhay81 <166787149+abhayabhay81@users.noreply.github.com>
2025-02-21 22:12:44 +0530

    Delete lecture 1/fruits directory

--- lecture 1/fruits/index.js
+++
@@ -1,7 +0,0 @@
-const apple = require("./apple")
-const banana = require("./banana")
-const papaya = require("./papaya")
-
-let info = [apple,banana,papaya]
-
-module.exports = info
--- lecture 1/fruits/papaya.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    name : "papaya",
-    color : "yellow"
-}
